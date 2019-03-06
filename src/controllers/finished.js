159493019c601caac992bdd1de7a300f8f5d12ae
ian-antking <ian.antking@gmail.com>
2019-03-06 15:34:01 +0000

    finished work now stored in appropriate bucket

--- src/controllers/finished.js
+++ src/controllers/finished.js
@@ -1,5 +1,5 @@
 const Finished = require('../models/finished');
-// const Minio = require('minio');
+const Minio = require('minio');
 
 exports.createFinished = (req, res) => {
   const fileData = JSON.parse(req.body.fileData);
@@ -10,7 +10,34 @@ exports.createFinished = (req, res) => {
     teacher: req.params.id,
   });
 
-  finished.save().then(() => {
-    res.status(201).json(finished);
+  const fileStorage = new Minio.Client({
+    endPoint: process.env.MINIO_SERVER,
+    port: 9000,
+    useSSL: false,
+    accessKey: process.env.MINIO_ACCESS,
+    secretKey: process.env.MINIO_SECRET,
+  });
+
+  const finishedBucket = process.env.MINIO_FINISHED_BUCKET;
+
+  fileStorage.putObject(finishedBucket, req.file.originalname, req.file.buffer, (err) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else {
+      finished.save().then(() => {
+        res.status(201).json(finished.toObject());
+      }).catch(error => {
+        if (error.name === 'ValidationError') {
+          const titleError = error.errors.title ? error.errors.title.message : null;
+          res.status(422).json({
+            errors: {
+              title: titleError,
+            },
+          });
+        } else {
+          res.sendStatus(500);
+        }
+      });
+    }
   });
 };
